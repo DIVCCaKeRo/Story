dc3d5aca89219e74ba05688cc6b8d540bfaebacf
Kevin898y <kevin888y@gmail.com>
2016-09-28 04:16:58 +0800

    Particle

--- Assets/Range.cs
+++ Assets/Range.cs
@@ -5,21 +5,40 @@ public class Range : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-      
+       
     }
-	
+    public GameObject explosion,temp;
 	// Update is called once per frame
 	void Update () {
         Vector3 Pos = gameObject.transform.position;
-        gameObject.transform.position = new Vector3(Mathf.Clamp(Pos.x, -4.7f, 4.7f), Mathf.Clamp(Pos.y, -0.2f, 1.7f), Mathf.Clamp(Pos.z,-4.7f ,0.3f ));             
+        gameObject.transform.position = new Vector3(Mathf.Clamp(Pos.x, -4.7f, 4.7f), Mathf.Clamp(Pos.y, -0.2f, 3.7f), Mathf.Clamp(Pos.z,-4.7f ,0.3f ));             
     }
-   // public GameObject Bullet;//子彈物件
     void OnCollisionEnter(Collision test) 
     {
         if (test.gameObject.name == "Cylinder") 
         {
-           // Instantiate(gameObject, new Vector3(0.0f,3.0f,0.0f), Quaternion.identity);
-            print("134564897987979488"); //在除錯視窗中顯示OK
+           temp= (GameObject)Instantiate(explosion, transform.position, Quaternion.identity);
+            
         }
     }
+    void OnCollisionExit(Collision aaa)
+    {
+        if (aaa.gameObject.name == "Cylinder")
+        {
+            Renderer renderer = aaa.gameObject.GetComponent<Renderer>();
+            renderer.material.color = Color.green;
+            Destroy(temp);
+        }
+    }
+    void OnCollisionStay(Collision aaa)
+    {
+        if (aaa.gameObject.name == "Cylinder")
+        {
+            Renderer renderer = aaa.gameObject.GetComponent<Renderer>();
+            renderer.material.color = Color.yellow;
+        }
+            
+
+    }
+    
 }
